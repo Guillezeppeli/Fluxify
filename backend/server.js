9da238a69507b5a7fd6292fefdba2f9ebccdabba
Guillermo Serrano <memo.scastillo@gmail.com>
2023-09-22 22:52:54 -0600

    Add CORS fix

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import './config.js'
 import express from 'express'
 import connectDB from './config/db.js'
+import cors from 'cors'
 import userRoutes from './routes/userRoutes.js'
 import productRoutes from './routes/productRoutes.js'
 import categoryRoutes from './routes/categoryRoutes.js'
@@ -11,6 +12,7 @@ const PORT = process.env.PORT || 5000
 connectDB()
 
 app.use(express.json())
+app.use(cors())
 
 app.use('/api/users', userRoutes)
 app.use('/api/products', productRoutes)
